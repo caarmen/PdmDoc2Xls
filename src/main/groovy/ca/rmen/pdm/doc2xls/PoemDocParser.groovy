79626c5731cf8dc5dedb885914d5fc5941f2c067
Carmen Alvarez <c@rmen.ca>
2015-03-01 00:04:27 +0100

    Making parser more robust

--- src/main/groovy/ca/rmen/pdm/doc2xls/PoemDocParser.groovy
+++ src/main/groovy/ca/rmen/pdm/doc2xls/PoemDocParser.groovy
@@ -6,7 +6,7 @@ package ca.rmen.pdm.doc2xls
 class PoemDocParser {
 
     private static String extractPageId(String input) {
-        def pattern = ~/^([a-z]) *[–-] ([A-Za-z]*) de ([a-z]+) de ([0-9]{4})/
+        def pattern = ~/^([a-z]) *[–-] (\p{L}*) de ([a-z]+) de ([0-9]{4})/
         def matcher = pattern.matcher(input)
         if (matcher.matches())
             return matcher.group(4) + "-" + matcher.group(3) + "-" + matcher.group(1)
@@ -30,11 +30,19 @@ class PoemDocParser {
     }
 
     private static String[] extractLocationDate(String input) {
-        def pattern = ~/^([\p{L} ]+), ([0-9]+) de (\p{L}*) de ([0-9]{4})/
+        def pattern = ~/^([\p{L} ]+)[\.,]? ([0-9]+) de (\p{L}*) (de )?([0-9]{4})[\. ]*/
         def matcher = pattern.matcher(input)
         if (matcher.matches())
-            return [matcher.group(1), matcher.group(4) + "-" + matcher.group(3) + "-" + matcher.group(2)]
+            return [matcher.group(1), matcher.group(5) + "-" + matcher.group(3) + "-" + matcher.group(2)]
         return null
+    }
+
+    private static boolean isPotentialPoemTitle(String input) {
+        if (input.isEmpty()
+                || input ==~ /^ *Breverías *$/
+                || input ==~ /^ *Imagenes *$/)
+            return false;
+        return true;
     }
 
     static Poem[] parse(String textFileName) {
@@ -44,15 +52,17 @@ class PoemDocParser {
             def line
             Poem curPoem
             String curPageId
+            int i;
             while ((line = reader.readLine()) != null) {
+                i++;
                 String pageId = extractPageId(line)
                 // This is the beginning of a new page
                 if (pageId != null) {
                     curPageId = pageId
                 } else if (curPageId != null) {
-                    // This is a new breveria
                     String breveriaId = extractBreveriaId(line)
                     if (breveriaId != null) {
+                        // This is a new breveria
                         curPoem = new Poem()
                         curPoem.type = Poem.PoemType.BREVERIA
                         curPoem.id = breveriaId
@@ -60,9 +70,9 @@ class PoemDocParser {
                         curPoem.pageId = curPageId
                         poems.add(curPoem)
                     } else {
-                        // This is a new sonnet
                         String[] sonnetId = extractSonnetId(line)
                         if (sonnetId != null) {
+                            // This is a new sonnet
                             curPoem = new Poem()
                             curPoem.type = Poem.PoemType.SONNET
                             curPoem.id = sonnetId[0]
@@ -70,10 +80,10 @@ class PoemDocParser {
                             curPoem.pageId = curPageId
                             poems.add(curPoem)
                         } else if (curPoem != null) {
-                            // This is the timestamp of the poem
-                            // (the end of the poem)
                             String[] locationDate = extractLocationDate(line)
                             if (locationDate != null) {
+                                // This is the timestamp of the poem
+                                // (the end of the poem)
                                 curPoem.location = locationDate[0]
                                 curPoem.date = locationDate[1]
                                 curPoem = null
@@ -81,12 +91,40 @@ class PoemDocParser {
                                 // This is content of the poem
                                 curPoem.content += line + "\n"
                             }
+                        } else if (isPotentialPoemTitle(line)) {
+                            curPoem = new Poem()
+                            curPoem.type = Poem.PoemType.POEM
+                            curPoem.title = line
+                            curPoem.pageId = curPageId
+                            poems.add(curPoem)
                         }
                     }
                 }
             }
-            for (poem in poems)
-                poem.content = poem.content.trim()
+        }
+        for (Poem poem : poems) {
+            poem.content = poem.content.trim()
+        }
+        for (Iterator<Poem> it = poems.iterator(); it.hasNext();) {
+            Poem poem = it.next();
+            if (poem.type != Poem.PoemType.POEM)
+                continue
+            // In case we accidentally parsed something that shouldn't
+            // be a poem, remove it.
+            if (poem.location == null || poem.date == null) {
+                println("Removing poem ${poem}")
+                it.remove()
+                continue
+            }
+
+            // We assume real poems will have at least 4 lines of text.
+            String content = poem.content.replaceAll(" ", "");
+            content = content.replaceAll("[\\n]+", "\n")
+            String[] lines = content.split("\n");
+            if (lines.length < 4) {
+                println("Removing poem ${poem}")
+                it.remove()
+            }
         }
         return poems
     }
